96e4996b27b4e48bce8218fefa4115cbc9c5eb98
rupkathasaha5-art <rupkathasaha5@gmail.com>
2025-12-08 20:09:20 +0530

    commit files for 04bgChanger project

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [color, setColor] = useState("white");
+
+  return (
+    <div className='w-full h-screen duration-200'
+    style={{backgroundColor:color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px=-3 py-2 rounded-xl'>
+          <button className='outline-none px-4 py-1 rounded-full text-white' style={{backgroundColor:"red"}} onClick={()=>{setColor("red")}}>red</button>
+          <button className='outline-none px-4 py-1 rounded-full text-white' style={{backgroundColor:"blue"}} onClick={()=>{setColor("blue")}}>blue</button>
+          <button className='outline-none px-4 py-1 rounded-full text-white' style={{backgroundColor:"green"}} onClick={()=>{setColor("green")}}>green</button>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
