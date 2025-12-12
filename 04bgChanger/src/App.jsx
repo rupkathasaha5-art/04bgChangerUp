0a9019fd8581fa7c41b4dc75bc2a623c5337433c
rupkathasaha5-art <rupkathasaha5@gmail.com>
2025-12-13 01:00:15 +0530

    add changed files

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -6,8 +6,7 @@ function App() {
   const [color, setColor] = useState("white");
 
   return (
-    <div className='w-full h-screen duration-200'
-    style={{backgroundColor:color}}>
+    <div className='w-full h-screen duration-200' style={{backgroundColor:color}}>
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px=-3 py-2 rounded-xl'>
           <button className='outline-none px-4 py-1 rounded-full text-white' style={{backgroundColor:"red"}} onClick={()=>{setColor("red")}}>red</button>
